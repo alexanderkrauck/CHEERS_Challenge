60da16b2f3861e6cb631c93d7dcfeece135d7887
Alexander Krauck <alexander.krauck@gmail.com>
2021-06-09 17:16:17 +0200

    Updated Relevant Module

--- CHEERS_challenge_round_1/utils/architectures.py
+++ CHEERS_challenge_round_1/utils/architectures.py
@@ -1,9 +1,12 @@
 import torch
 from torch import nn
+from torch._C import device, dtype
 import torch.nn.functional as F
 
 from pytorch_lightning import LightningModule
 import numpy as np
+
+import sklearn.metrics as sm
 
 from transformers import BertModel
 
@@ -208,6 +211,7 @@ class RelevantModuleV03(LightningModule):
         super().__init__()        
         self.bert = bert
         self.linear_after_bert = nn.Linear(bert.config.hidden_size, 256)
+
         self.feed_forward = nn.Sequential(
             nn.Linear(256 + input_size, 1024),
             nn.ReLU(),
@@ -234,11 +238,12 @@ class RelevantModuleV03(LightningModule):
         x_bert = x[0]
         x_other = x[1]
 
-        x_bert = self.bert(x_bert)["last_hidden_state"][:,0] #all batches but only clf output
+        x_bert = self.bert(x_bert)["last_hidden_state"][:,0] #all batches but only [CLF] output
         x_bert = self.linear_after_bert(x_bert)
         x_bert = torch.relu(x_bert)#is new (not sure if improves by much)
 
         x = torch.cat((x_bert, x_other), dim=1)#dim=1 is feature dimensions (0 is batch dim)
+        x = x.float()
 
         return self.feed_forward(x)
 
@@ -248,7 +253,7 @@ class RelevantModuleV03(LightningModule):
         return {
            'optimizer': optimizer,
            'lr_scheduler': torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True, factor =.2, patience =1, cooldown =2, min_lr =1e-6),
-           'monitor': 'val_loss'
+           'monitor': 'f1_support'
        }
        
     def training_step(self, train_batch, batch_idx):
@@ -264,36 +269,38 @@ class RelevantModuleV03(LightningModule):
 
     def validation_step(self, val_batch, batch_idx):
         x, y = val_batch
+
         y_hat = self.forward(x)
         loss_weights = self.class_weighting[y.data.view(-1).long()].view_as(y_hat)
         losses = F.binary_cross_entropy_with_logits(y_hat.flatten(), y.float(), reduction = "none")#BCE needs y to be float
         loss = torch.mean(loss_weights * losses)
         y_hat = torch.round(torch.sigmoid(y_hat.flatten()))
-        p = y_hat[y==1] == y[y==1]
-        n = y_hat[y==0] == y[y==0]
-        acc = torch.sum(y_hat == y) / len(y)
+        
+        y_det = y.detach().cpu().numpy()
+        y_hat_det = y_hat.detach().cpu().numpy()
+        acc = sm.accuracy_score(y_det, y_hat_det)
 
         self.log('val_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
         self.log("val_acc", acc, on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
-        return (p*1, n*1)
+        return (y_det, y_hat_det)
     
     def validation_epoch_end(self, validation_step_outputs):
-        ps = [p.cpu().numpy() for (p, n) in validation_step_outputs]
-        ns = [n.cpu().numpy() for (p, n) in validation_step_outputs]
-        ps = np.concatenate(ps)
-        ns = np.concatenate(ns)
-        if len(ps) != 0:
-            tpr = sum(ps)/len(ps)
-        else:
-            tpr = 1
-        tnr = sum(ns)/len(ns)
+        y = [y for (y, y_hat) in validation_step_outputs]
+        y_hat = [y_hat for (y, y_hat) in validation_step_outputs]
+        y = np.concatenate(y)
+        y_hat = np.concatenate(y_hat)
+        
 
-        bacc = (tpr + tnr)/2
+        bacc = sm.balanced_accuracy_score(y, y_hat)
+        precision, recall, f1_score_relevance, _ = sm.precision_recall_fscore_support(y, y_hat, average="macro")
 
-        self.log('tpr', tpr, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        self.log('tnr', tnr, on_step=False, on_epoch=True, prog_bar=False, logger=True)
         self.log('bacc', bacc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log('f1_support', f1_score_relevance, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log('precision', precision, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log('recall', recall, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+
+
 
 
 
